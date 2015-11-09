1f161b069c235e67bb375e6db2d5d7138ff23a12
Jason Miller <jason@developit.ca>
2015-11-08 20:45:54 -0500

    Add more react-friendly support for className attributes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import PropTypes from 'proptypes';
-import { render, h, Component } from 'preact';
+import { render, h, Component, hooks } from 'preact';
 
 let createElement = h;
+
+// make className work like React's
+let oldHook = hooks.vnode;
+
+hooks.vnode = vnode => {
+	if (oldHook) vnode = oldHook(vnode) || vnode;
+	let { attributes } = vnode;
+	if (attributes) {
+		let cl = attributes.className || attributes.class;
+		if (cl) {
+			attributes.className = cl;
+		}
+	}
+};
 
 /** @TODO subclass preact's Component to do basic proptype checking. */
 
