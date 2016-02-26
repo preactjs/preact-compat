ce01d872700ff66168922dda86f22d5285787e32
Jason Miller <jason@developit.ca>
2016-02-25 20:43:07 -0500

    Add comprehensive tests for String and Function refs.

--- test/component.js
+++ test/component.js
@@ -4,6 +4,22 @@ import React from '../src';
 /*global sinon,expect*/
 
 describe('components', () => {
+	let scratch;
+
+	before( () => {
+		scratch = document.createElement('div');
+		(document.body || document.documentElement).appendChild(scratch);
+	});
+
+	beforeEach( () => {
+		scratch.innerHTML = '';
+	});
+
+	after( () => {
+		scratch.parentNode.removeChild(scratch);
+		scratch = null;
+	});
+
 	it('should be exported', () => {
 		let props;
 
@@ -26,5 +42,99 @@ describe('components', () => {
 		});
 
 		expect(html).to.equal('<div id="demo">inner</div>');
+	});
+
+	describe('refs', () => {
+		it('should support string refs', () => {
+			let inst, innerInst;
+
+			class Foo extends React.Component {
+				constructor() {
+					super();
+					inst = this;
+				}
+
+				render() {
+					return (
+						<div ref="top">
+							<h1 ref="h1">h1</h1>
+							<p ref="p">
+								<span ref="span">text</span>
+								<Inner ref="inner" />
+							</p>
+						</div>
+					);
+				}
+			}
+
+			const Inner = React.createClass({
+				render() {
+					innerInst = this;
+					return (
+						<div className="contained" ref="contained">
+							<h2 ref="inner-h2">h2</h2>
+						</div>
+					);
+				}
+			});
+
+			React.render(<Foo />, scratch);
+
+			expect(inst).to.exist;
+			expect(inst.refs).to.be.an.object;
+
+			expect(inst.refs).to.have.property('top', scratch.firstChild);
+			expect(inst.refs).to.have.property('h1', scratch.querySelector('h1'));
+			expect(inst.refs).to.have.property('p', scratch.querySelector('p'));
+			expect(inst.refs).to.have.property('span', scratch.querySelector('span'));
+
+			expect(inst.refs).to.have.property('inner', innerInst, 'ref to child component');
+
+			expect(inst.refs).not.to.have.property('contained');
+			expect(inst.refs).not.to.have.property('inner-h2');
+
+			expect(innerInst.refs).to.have.all.keys(['contained', 'inner-h2']);
+			expect(innerInst.refs).to.have.property('contained', scratch.querySelector('.contained'));
+			expect(innerInst.refs).to.have.property('inner-h2', scratch.querySelector('h2'));
+		});
+
+		it('should retain support for function refs', () => {
+			let ref1 = sinon.spy(),
+				ref2 = sinon.spy(),
+				componentRef = sinon.spy(),
+				innerInst;
+
+			class Foo extends React.Component {
+				render() {
+					return this.props.empty ? (<foo />) : (
+						<div ref={ref1}>
+							<h1 ref={ref2}>h1</h1>
+							<Inner ref={componentRef} />
+						</div>
+					);
+				}
+			}
+
+			const Inner = React.createClass({
+				render() {
+					innerInst = this;
+					return <div />;
+				}
+			});
+
+			React.render(<Foo />, scratch);
+
+			expect(ref1).to.have.have.been.calledOnce.and.calledWith(scratch.firstChild);
+			expect(ref2).to.have.have.been.calledOnce.and.calledWith(scratch.querySelector('h1'));
+			expect(componentRef).to.have.been.calledOnce.and.calledWith(innerInst);
+
+			React.render(<Foo empty />, scratch);
+
+			expect(ref1).to.have.have.been.calledTwice.and.calledWith(null);
+			expect(ref2).to.have.have.been.calledTwice.and.calledWith(null);
+
+			// @BUG: component unmount not nulling ref?
+			//expect(componentRef).to.have.been.calledTwice.and.calledWith(null);
+		});
 	});
 });
