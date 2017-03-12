a4624cdef9af217c146f28ce5ce6fe084abed775
Jason Miller <jason@developit.ca>
2017-03-12 12:08:52 -0400

    Split up ESM and UMD builds to selectively enable default stripping only for UMD. Fixes #321. Also fixes #320.

--- rollup.config.js
+++ rollup.config.js
@@ -8,18 +8,23 @@ let pkg = JSON.parse(fs.readFileSync('./package.json'));
 
 let external = Object.keys(pkg.peerDependencies || {}).concat(Object.keys(pkg.dependencies || {}));
 
+let format = process.env.FORMAT==='es' ? 'es' : 'umd';
+
 export default {
 	entry: 'src/index.js',
 	sourceMap: true,
-	exports: 'default',
-	useStrict: false,
+	moduleName: pkg.amdName,
+	exports: format==='es' ? null : 'default',
+	dest: format==='es' ? pkg.module : pkg.main,
+	format,
 	external,
+	useStrict: false,
 	globals: {
 		'preact': 'preact',
 		'proptypes': 'PropTypes'
 	},
 	plugins: [
-		memory({
+		format==='umd' && memory({
 			path: 'src/index.js',
 			contents: "export { default } from './index';"
 		}),
@@ -36,9 +41,5 @@ export default {
 			include: 'node_modules/**',
 			exclude: '**/*.css'
 		})
-	],
-	targets: [
-		{ dest: pkg.main, format: 'umd', moduleName: pkg.amdName },
-		{ dest: pkg.module, format: 'es' }
-	]
+	].filter(Boolean)
 };
