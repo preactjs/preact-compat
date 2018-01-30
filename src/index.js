preactjs/preact-compat

commit e251afb9dd3b857096cbbe299588b1d4b3ae6f27

Add unstable_batchedUpdates to exported functions (#411) (#289)

src/index.js
@@ -591,6 +591,10 @@ PureComponent.prototype.shouldComponentUpdate = function(props, state) {
 	return shallowDiffers(this.props, props) || shallowDiffers(this.state, state);
 };
 
+function unstable_batchedUpdates(callback) {
+  callback();
+}
+
 export {
 	version,
 	DOM,
@@ -607,8 +611,7 @@ export {
 	Component,
 	PureComponent,
 	renderSubtreeIntoContainer as unstable_renderSubtreeIntoContainer,
-	// this is a really old hidden react api, but something out there uses it
-	// https://twitter.com/Joseph_Wynn/status/888046593286574085
+	unstable_batchedUpdates,
 	extend as __spread
 };
 
@@ -628,5 +631,6 @@ export default {
 	Component,
 	PureComponent,
 	unstable_renderSubtreeIntoContainer: renderSubtreeIntoContainer,
+	unstable_batchedUpdates,
 	__spread: extend
 };

commit 7ebe07400cd6f27ea4bfcdc59c6b98965394ebde

fix eslint issue
(introduced by #289)

src/index.js
@@ -594,7 +594,7 @@ PureComponent.prototype.shouldComponentUpdate = function(props, state) {
 };
 
 function unstable_batchedUpdates(callback) {
-  callback();
+	callback();
 }
 
 export {