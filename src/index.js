19f5370eaa903353eaec3a6381bbad98380b226a
Christian Kaisermann <christian@kaisermann.me>
2018-05-29 00:29:50 -0300

    Make process shim not necessary on production environments

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const CAMEL_PROPS = /^(?:accent|alignment|arabic|baseline|cap|clip|color|fill|fl
 const BYPASS_HOOK = {};
 
 /*global process*/
-const DEV = typeof process==='undefined' || !process.env || process.env.NODE_ENV!=='production';
+const DEV = typeof process !== 'undefined' && process.env.NODE_ENV!=='production';
 
 // a component that renders nothing. Used to replace components for unmountComponentAtNode.
 function EmptyComponent() { return null; }
