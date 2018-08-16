d81e2c17606cc3b987824eeb6a20907debc65393
Jason Miller <developit@users.noreply.github.com>
2018-08-15 20:15:08 -0400

    Add createRef
    Implemented in developit/preact#1138, we just have to export it here.
    This won't do anything until Preact 8.4.0 is released.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { render as preactRender, cloneElement as preactCloneElement, h, Component as PreactComponent, options } from 'preact';
+import { render as preactRender, cloneElement as preactCloneElement, createRef, h, Component as PreactComponent, options } from 'preact';
 
 const version = '15.1.0'; // trick libraries to think we are react
 
@@ -614,6 +614,7 @@ export {
 	createFactory,
 	createElement,
 	cloneElement,
+	createRef,
 	isValidElement,
 	findDOMNode,
 	unmountComponentAtNode,
@@ -635,6 +636,7 @@ export default {
 	createFactory,
 	createElement,
 	cloneElement,
+	createRef,
 	isValidElement,
 	findDOMNode,
 	unmountComponentAtNode,
